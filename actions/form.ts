b1a7a13ce642947c36efd348f3721863b1a8b399
AltafHunzai <ah5509092@gmail.com>
2024-02-26 10:32:45 +0500

    publish form is enabled now

--- actions/form.ts
+++ actions/form.ts
@@ -111,3 +111,19 @@ export async function UpdateFormContent(id: number, JsonContent: string) {
     },
   });
 }
+
+export async function PublishForm(id: number) {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+  return await prisma.form.update({
+    data: {
+      published: true,
+    },
+    where: {
+      userId: user.id,
+      id,
+    },
+  });
+}
--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -1,15 +1,78 @@
-import React from "react";
+import React, { useTransition } from "react";
 import { Button } from "./ui/button";
 import { MdOutlinePublish } from "react-icons/md";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { FaIcons } from "react-icons/fa";
+import { toast } from "./ui/use-toast";
+import { PublishForm } from "@/actions/form";
+import { useRouter } from "next/navigation";
 
-function PublishFormBtn() {
+function PublishFormBtn({ id }: { id: number }) {
+  const [loading, startTransition] = useTransition();
+  const router = useRouter();
+
+  async function publishForm() {
+    try {
+      await PublishForm(id);
+      toast({
+        title: "Success",
+        description: "Your form is now available to the public",
+      });
+      router.refresh();
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Went something wrong",
+        variant: "destructive",
+      });
+    }
+  }
   return (
-    <Button
-      variant={"outline"}
-      className='gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400 hover:from-cyan-400 hover:to-indigo-400'
-    >
-      <MdOutlinePublish className='h-4 w-4' /> Publish
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant={"outline"}
+          className='gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400 hover:from-cyan-400 hover:to-indigo-400'
+        >
+          <MdOutlinePublish className='h-4 w-4' /> Publish
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutly sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. After publishing you will not able to
+            edit this form. <br /> <br />{" "}
+            <span className='font-medium'>
+              By publishing this form you will make it available to the public
+              and you will be able to collect submissions.
+            </span>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={loading}
+            onClick={(e) => {
+              e.preventDefault();
+              startTransition(publishForm);
+            }}
+          >
+            Proceed {loading && <FaIcons className=' animate-spin' />}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
