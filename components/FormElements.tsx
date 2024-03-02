5b4f262d87f03e6e5c7e1c06be207481c0c0269b
AltafHunzai <ah5509092@gmail.com>
2024-03-02 14:02:38 +0500

    applied validation logic for the form & form submit

--- components/FormElements.tsx
+++ components/FormElements.tsx
@@ -2,6 +2,8 @@ import { TextFieldFormElement } from "./fields/TextField";
 
 export type ElementsType = "TextField";
 
+export type SubmitFunction = (key: string, value: string) => void;
+
 export type FormElement = {
   type: ElementsType;
 
@@ -17,10 +19,14 @@ export type FormElement = {
   }>;
   formComponent: React.FC<{
     elementInstance: FormElementInstance;
+    submitValue?: (key: string, value: string) => void;
+    IsInvalid?: boolean;
+    defaultValue?: string;
   }>;
   propertiesComponent: React.FC<{
     elementInstance: FormElementInstance;
   }>;
+  validate: (FormElement: FormElementInstance, currentValue: string) => boolean;
 };
 export type FormElementInstance = {
   id: string;
--- components/FormSubmitContent.tsx
+++ components/FormSubmitContent.tsx
@@ -2,7 +2,10 @@
 import { HiCursorClick } from "react-icons/hi";
 import { FormElementInstance, FormElements } from "./FormElements";
 import { Button } from "./ui/button";
-import { useCallback, useRef } from "react";
+import { useCallback, useRef, useState, useTransition } from "react";
+import { toast } from "./ui/use-toast";
+import { ImSpinner2 } from "react-icons/im";
+import { SubmitForm } from "@/actions/form";
 
 function FormSubmitContent({
   formUrl,
@@ -11,21 +14,104 @@ function FormSubmitContent({
   formUrl: string;
   content: FormElementInstance[];
 }) {
+  const formValues = useRef<{ [key: string]: string }>({});
+  const formErrors = useRef<{ [key: string]: boolean }>({});
+  const [renderKey, setRenderKey] = useState(new Date().getTime());
+
+  const [submitted, setSubmitted] = useState(false);
+  const [pending, startTransition] = useTransition();
+
+  const validateForm: () => boolean = useCallback(() => {
+    for (const field of content) {
+      const actualValue = formValues.current[field.id] || "";
+      const valid = FormElements[field.type].validate(field, actualValue);
+      if (!valid) {
+        formErrors.current[field.id] = true;
+      }
+    }
+
+    if (Object.keys(formErrors).length > 0) {
+      return false;
+    }
+    return true;
+  }, [content]);
+
+  const submitValue = useCallback((key: string, value: string) => {
+    formErrors.current = {};
+    const validForm = validateForm();
+    formValues.current[key] = value;
+  }, []);
+
+  const submitForm = async () => {
+    formErrors.current = {};
+    const validForm = validateForm();
+    if (!validForm) {
+      setRenderKey(new Date().getTime());
+      toast({
+        title: "Error",
+        description: "Please check the form for errors",
+        variant: "destructive",
+      });
+    }
+
+    try {
+      const jsonContent = JSON.stringify(formValues.current);
+      await SubmitForm(formUrl, jsonContent);
+      setSubmitted(true);
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong",
+        variant: "destructive",
+      });
+    }
+    console.log("FORM VALUE", formValues.current);
+  };
+
+  if (submitted) {
+    return (
+      <div className='flex justify-center w-full h-full items-center p-8'>
+        <div className='max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-auto border shadow-xl shadow-blue-700 rounded'>
+          <h1 className="text-2xl font-bold">Form Submitted</h1>
+          <p className="text-muted-foreground">Thank you for submitting the form, you can close this page now.</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className='flex justify-center w-full h-full items-center p-8'>
-      <div className='max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-auto border shadow-xl shadow-blue-700 rounded'>
+      <div
+        key={renderKey}
+        className='max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-auto border shadow-xl shadow-blue-700 rounded'
+      >
         {content.map((element) => {
           const FormElement = FormElements[element.type].formComponent;
           return (
             <FormElement
               key={element.id}
               elementInstance={element}
+              submitValue={submitValue}
+              IsInvalid={formErrors.current[element.id]}
+              defaultValue={formValues.current[element.id]}
             />
           );
         })}
-        <Button className='mt-8'>
-          <HiCursorClick className='mr-2' />
-          Submit
+        <Button
+          className='mt-8'
+          onClick={() => {
+            startTransition(submitForm);
+            submitForm();
+          }}
+          disabled={pending}
+        >
+          {!pending && (
+            <>
+              <HiCursorClick className='mr-2' />
+              Submit
+            </>
+          )}
+          {pending && <ImSpinner2 />}
         </Button>
       </div>
     </div>
